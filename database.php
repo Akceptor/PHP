77a40143b3be7002a9b6c478c557d8794272d32c
Volodymyr Ostapiv <akceptor.spam@gmail.com>
2013-04-01 12:53:10 +0300

    separated DB updates

--- database.php
+++ database.php
@@ -6,15 +6,14 @@ echo "</head><body>";
 
 if (isset($_SESSION['author'])) {
     $author_id = $_SESSION['author'];
-    echo "<H1>Ваш юзерайді:" . $author_id . "</H1>";
-    echo "<a href='http://localhost'>Logout</a>";
+    echo " Ваш юзерайді: " . $author_id . "     <a href='http://localhost'>Вийти</a><br>";
 } else {
     die('Доступ заборонено!');
 }
 ?>
 	
-	<?php include 'db.php'?>
-
+	<?php include 'db.php' ?>
+	<?php include 'antispam.php' ?>
 <!-- DB Connect -->
 <?php
 $connect = mysql_connect(HOST, USER, PASSWORD) or
@@ -28,7 +27,18 @@ mysql_select_db(NAME_DB, $connect) or
 mysql_query("SET character_set_connection=utf8");
 mysql_query("SET character_set_client=utf8");
 mysql_query("SET character_set_results=utf8");
+
+$result = mysql_query("SELECT COUNT(id) AS count FROM `my_table`");
+$row = mysql_fetch_assoc($result);
+$rows_max = $row['count']; // Records quantity
+$show_pages = max(1, $_GET['pages']); // Records per page
+$this_page = filter_var($_GET['page'], FILTER_SANITIZE_NUMBER_INT);
+echo "Всього записів у базі: ".$rows_max;
+echo "<br>Записів на сторінку: ".$show_pages;
+echo "<br>Поточна сторінка: ".max(1,$this_page);
 ?>
+
+
 <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
 	<!-- Get all query -->
 	<?php
@@ -39,17 +49,48 @@ mysql_query("SET character_set_results=utf8");
  * @version 1.0
  *         
  */
-$sql = "SELECT * FROM my_table";
+if ($this_page)
+{
+        $offset = (($show_pages * $this_page) - $show_pages);
+}
+else
+{
+        $this_page = 1; // set first page $_GET['page']
+	$offset = 0;
+}
+echo'<center> ';
+// Page navi
+echo '<a href="?page=1"> << </a>';//goto 1st page
+if ($rows_max > $show_pages)
+{
+       $r = 1;
+       while ($r <= ceil($rows_max/$show_pages))
+       {
+           if ($r != $this_page)
+           {          
+                echo '<a href="?page=' . $r . '&pages='.$show_pages.'"> '. $r .'</a>';
+           }
+           else
+           {
+               echo '<b>' . $r . '</b>'; // Current page
+            }
+            $r++;      
+       }
+}
+echo '<a href="?page='.ceil($rows_max/$show_pages).'"> >> </a>';//goto last page
+echo'</center> <br>';
+$sql = "select * from my_table ORDER BY id ASC LIMIT $offset, $show_pages";
 $result = mysql_query($sql) or die(mysql_error());
-$table = "<table border=1>";
+
+$table = "<table border=1 align='center'>";
 $table .= "<tr align='center'>";
-$table .= "<td> ID </td><td> NAME </td><td> SHORT </td><td> FULL </td><td> CREATE </td><td> EDIT </td><td colspan=2> BUTTONS </td></tr>";
+$table .= "<td></td><td> NAME </td><td> SHORT </td><td> FULL </td><td> CREATE </td><td> EDIT </td><td colspan=2> BUTTONS </td></tr>";
 while ($row = mysql_fetch_assoc($result)) {
     $table .= "<tr>"; // show records from db
-    $table .= "<td><input type = 'text' name='id[]' id='id' size = 3 value='" .
-             $row['id'] . "'></td>";
+    $table .= "<td><input type = 'text' name='id[]' id='id' size = 1 value='" .
+             $row['id'] . "' hidden></td>";
     $table .= "<td><input type = 'text' name='name[]' id='name' size = 10 value='" .
-             $row['name'] . "'></td>";
+             $row['name'] . "' ></td>";
     $table .= "<td><input type = 'text' name='short[]' id='short' size = 20 value='" .
              $row['shorttext'] . "'></td>";
     $table .= "<td><textarea name='full[]' id='full' rows = 3 cols = 50 >" .
@@ -66,9 +107,9 @@ while ($row = mysql_fetch_assoc($result)) {
 }
 $table .= "<tr>"; // add new record
 $table .= "<td><input type = 'text' name='addid' id='addid' size = 3 value='' hidden></td>";
-$table .= "<td><input type = 'text' name='addname' id='addname' size = 10 value=''></td>";
-$table .= "<td><input type = 'text' name='addshort' id='addshort' size = 20 value=''></td>";
-$table .= "<td><textarea name='addfull' id='addfull' rows = 3 cols = 50 ></textarea></td>";
+$table .= "<td><input type = 'text' name='addname' id='addname' size = 10 value='' placeholder='Як Вас звати?' ></td>";
+$table .= "<td><input type = 'text' name='addshort' id='addshort' size = 20 value='' placeholder='Коротка інфа' ></td>";
+$table .= "<td><textarea name='addfull' id='addfull' rows = 3 cols = 50 placeholder='Опишіть ситуацію повністю' ></textarea></td>";
 $table .= "<td><input type = 'date' name='addcreation' id='addcreation' size = 8 value='" .
          $date . "'></td>";
 $table .= "<td><input type = 'date' name='addedition' id='addedition' size = 8 value='" .
@@ -81,70 +122,15 @@ echo $table;
 ?>
 	
 	</form>
+<form name='recform' action='' method='get'></br>
+Показувати по: <select name='pages'><option value=3>3</option><option value=5>5</option><option value=7>7</option></select> записів
+<button type='submit'>Так!</button></form>
 <center>
 	<a href="<?php $_SERVER['PHP_SELF']?>">RELOAD</a>
 </center>
 <!-- add data to DB -->
-<?php
-/**
- * Add\edit\delete stuff
- *
- * @author Akceptor <akceptor.spam@gmail.com>
- * @version 1.0
- *         
- */
 
-$add = $_POST['add'];
-if ($add) {
-    mysql_query(
-            "INSERT INTO my_table (`name`, `shorttext`, `fulltext`, `creation_date`, `edit_date`, `author_id`) VALUES ('$_POST[addname]','$_POST[addshort]','$_POST[addfull]','$_POST[addcreation]','$_POST[addedition]', '$author_id')") or
-             die("Error inserting to db" . mysql_error());
-    // reload window
-     echo "<script type='text/javascript'>
-     parent.window.location.reload(true);</script>";
-}
-$edit = $_POST['edit'];
-if ($edit) {
-    //check author
-    $sql="SELECT `author_id` FROM my_table WHERE `id`=".$edit." LIMIT 1";
-    $result = mysql_query($sql);
-    if  ($author_id == mysql_result($result, 0)){
-    // create UPDATE query
-    $sql = "UPDATE my_table SET `name`='" .
-             $_POST[name][array_search($edit, $_POST['id'])] . "', `shorttext`='" .
-             $_POST[short][array_search($edit, $_POST['id'])] . "', `fulltext`='" .
-             $_POST[full][array_search($edit, $_POST['id'])] .
-             "', `creation_date`='" .
-             $_POST[creation][array_search($edit, $_POST['id'])] .
-             "', `edit_date`='" .
-             $_POST[edition][array_search($edit, $_POST['id'])] . "' WHERE `id`=" .
-             $edit;
-    // execute UPDATE query
-    mysql_query($sql) or die("Error updating DB" . mysql_error());
-    } else { 
-        die ("No rights to do that");
-    }
-    // reload window
-     echo "<script type='text/javascript'>
-     parent.window.location.reload(true);</script>";
-}
-$delete = $_POST['del'];
-if ($delete) {
-    //check author
-    $sql="SELECT `author_id` FROM my_table WHERE `id`=".$delete." LIMIT 1";
-    $result = mysql_query($sql);
-    if  ($author_id == mysql_result($result, 0)){
-    // create and execute DELETE query
-    mysql_query("DELETE FROM my_table WHERE `id`=" . $delete) or
-             die("Error deleting from DB" . mysql_error());
-    } else {
-        die ("No rights to do that");
-    }
-    // reload window
-     echo "<script type='text/javascript'>
-     parent.window.location.reload(true);</script>";
-}
-?>
+<?php include 'persistent.php' ?>
 
 <!-- close DB -->
 <?php
